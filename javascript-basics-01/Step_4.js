056caf8f90b1b0fc5c816828cd900c756c72a74d
LeaZN <zoueinlea@gmail.com>
2021-05-30 13:32:27 +0300

    Completed_Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,47 @@
+
+
+    window.onload = function(){
+        document.getElementById('validate').onclick = function(){
+            alert("Name: "+document.getElementById("name").value+"\r\n"+"Surname: "+document.getElementById("surname").value+"\r\n"+"City: "+document.getElementById("city").value);
+            return false;
+        }
+     }
+
+
+
+     //draft_Trials:
+
+
+
+
+// function myFunction() {
+//     var txt;
+//     if (submit("Press a button!")) {
+//         txt = "You pressed OK!";
+//     } else {
+//         txt = "You pressed Cancel!";
+//     }
+
+
+
+
+    // alert("Name: "+name+"\r\n"+"Surname: "+surname+"\r\n"+"City: "+city);
+    // function (){
+    //     if ()
+    // }
+    // alert("Thank you! You entered:"+"\r\n"+"Name: "+document.form.name.value+"\r\n"+"Surname: "+document.form.surname.value+"\r\n"+"City: "+document.form.city.value);
+
+    // var name = document.getElementById("name").value;
+    // var surname = document.getElementById("surname").value;
+    // var city = document.getElementById("city").value;
+   
+
+    // window.onload = function(){
+    //     document.getElementById('validate').onclick = function() {
+            
+        
+    //         alert("Thank you! You entered:"+"\r\n"+"Name: "+name+"\r\n"+"Surname: "+surname+"\r\n"+"City: "+city);
+    //         return false;
+    
+    // }
+
