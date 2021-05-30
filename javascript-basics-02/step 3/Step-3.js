fa1f67fefdda380438bb55ddbfb3b094cd3bcda5
LeaZN <zoueinlea@gmail.com>
2021-05-30 18:24:07 +0300

    Uncomplete_Step_3

---
+++ javascript-basics-02/step 3/Step-3.js
@@ -0,0 +1,16 @@
+// var newDiv = document.createElement("div");
+
+// var div = document.querySelector("div");
+// var input = document.querySelector("input");
+
+
+// function updateText(input){
+//     document.div.innerHTML =text;
+//     }
+var newDiv = document.createElement("div");
+function myFunction()
+    {
+        var input = document.querySelector("input");
+        var div = document.querySelector("div");
+        div.innerHTML = div.innerHTML + input.value;
+    }
