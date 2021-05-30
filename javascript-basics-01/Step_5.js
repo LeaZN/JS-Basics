abdd5b2bd70858d5a7885be3c8d8c7d6fc2bbecf
LeaZN <zoueinlea@gmail.com>
2021-05-30 14:42:00 +0300

    Completed_Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function Multiply() {
+    var a = document.getElementById("first_number").value;
+    var b = document.getElementById("second_number").value;
+    var result = parseInt(a) * parseInt(b);
+    alert(result);
+}
