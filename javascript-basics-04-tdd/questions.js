3b22f062888f4f3f4c3e1729e0c6b69d11dc360f
LeaZN <zoueinlea@gmail.com>
2021-05-31 09:38:15 +0300

    Partially_Done_Part_4

--- javascript-basics-04-tdd/questions.js
+++ javascript-basics-04-tdd/questions.js
@@ -1,21 +1,22 @@
 
 const stringSize = (text) => {
-
+return text.length;
 }
 const replaceCharacterE = (text) => {
-
+let replace = text.replace ("e", " " );
+return replace;
 }
 const concatString = (text1, text2) => {
-
+return text1.concat (text2)
 }
 const showChar5 = (text) => {
-
+return text.charAt(4);
 }
 const showChar9 = (text) => {
-
+return text.slice(0,9);
 }
 const toCapitals = (text) => {
-
+return text.toUpperCase();
 }
 const toLowerCase = (text) => {
 
